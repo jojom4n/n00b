20879cfee91bb28680a8093118402ed95e435aff
sarum4n <jojom4n@gmx.com>
2019-02-06 02:20:31 +0100

    Bugfix in defs.h

--- n00b/defs.h
+++ n00b/defs.h
@@ -4,19 +4,12 @@
 // definitions for bitboard "bb" (see Board.h)
 constexpr bool white = 0;
 constexpr bool black = 1;
-
-constexpr unsigned short W_King = 0;
-constexpr unsigned short W_Queen = 1;
-constexpr unsigned short W_Rooks = 2;
-constexpr unsigned short W_Knights = 3;
-constexpr unsigned short W_Bishops = 4;
-constexpr unsigned short W_Pawns = 5;
-constexpr unsigned short B_King = 6;
-constexpr unsigned short B_Queen = 7;
-constexpr unsigned short B_Rooks = 8;
-constexpr unsigned short B_Knights = 9;
-constexpr unsigned short B_Bishops = 10;
-constexpr unsigned short B_Pawns = 11;
+constexpr unsigned short king = 0;
+constexpr unsigned short queen = 1;
+constexpr unsigned short rooks = 2;
+constexpr unsigned short knights = 3;
+constexpr unsigned short bishops = 4;
+constexpr unsigned short pawns = 5;
 
 // definitions for commands.cpp
 enum Options { quit, help, fen, play, sysinfo, display, invalid };

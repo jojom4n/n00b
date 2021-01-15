520e12a5329d9f0081097a04a64588f27e2d29c1
sarum4n <jojom4n@gmx.com>
2021-01-15 02:30:15 +0100

    PVS from the scratch (legacyPVS)

--- n00b/params.h
+++ n00b/params.h
@@ -6,5 +6,8 @@
 constexpr unsigned long long PERFT_CACHE_SIZE = 0x2DC6C0; // perft cache size (in number of positions)
 constexpr unsigned long long TT_SIZE = 1024 * TT_BIT; // megabytes dedicated to TT
 
+#define R 2 // for null-move pruning
+#define MARGIN 200 // for futility pruning
+#define MAX_PLY 99
 
 #endif
--- n00b/search.cpp
+++ n00b/search.cpp
@@ -41,8 +41,8 @@ const Move iterativeSearch(Position& p, short const& depth)
 			mySearch.ttUseful = 0;
 
 			auto depthTimeStart = Clock::now();
-			// mySearch.bestScore = negamaxAB<false>(mySearch.pos, ply, ALPHA, BETA, mySearch.pv);
-			mySearch.bestScore = pvs<false>(mySearch.pos, ply, ALPHA, BETA, mySearch.pv);
+			// mySearch.bestScore = pvs<false>(mySearch.pos, ply, ALPHA, BETA, mySearch.pv);
+			mySearch.bestScore = legacyPVS(mySearch.pos, ply, ALPHA, BETA, mySearch.pv);
 			auto depthTimeEnd = Clock::now();
 
 			std::chrono::duration<float, std::milli> depthTime = depthTimeEnd - depthTimeStart;
@@ -212,71 +212,103 @@ const short pvs(Position const& p, short const& depth, short alpha, short beta, 
 	}
 
 
-	/* ********************************************************* */
-	/*                        PV NODE                            */
-	/*                                                           */
-	/*              Ordinary search in PV node                   */
-	/*                                                           */
-	/* ********************************************************* */
-	if (moveList.size() > 1) {
-		doMove(moveList[0], copy);
-		mySearch.nodes++;
-		bestMove = moveList[0];
-		bestScore = -pvs<false>(copy, depth - 1, -beta, -alpha, subPV);
-		undoMove(moveList[0], copy, p);
+	///*
+	///* ********************************************************* */
+	///*                        PV NODE                            */
+	///*                                                           */
+	///*              Ordinary search in PV node                   */
+	///*                                                           */
+	///* ********************************************************* */
+	//if (moveList.size() > 1) {
+	//	doMove(moveList[0], copy);
+	//	mySearch.nodes++;
+	//	bestMove = moveList[0];
+	//	bestScore = -pvs<false>(copy, depth - 1, -beta, -alpha, subPV);
+	//	undoMove(moveList[0], copy, p);
 
-		if (bestScore > alpha) {
-			
-			if (bestScore >= beta)
-				return bestScore;
-				
-			pv[0] = moveList[0];
-			memcpy(pv + 1, subPV, 63 * sizeof(Move));
-			pv[63] = 0;
-			alpha = bestScore;
-		}
+	//	if (bestScore > alpha) {
+	//		
+	//		if (bestScore >= beta)
+	//			return bestScore;
+	//			
+	//		pv[0] = moveList[0];
+	//		memcpy(pv + 1, subPV, 63 * sizeof(Move));
+	//		pv[63] = 0;
+	//		alpha = bestScore;
+	//	}
 
-		moveList.erase(moveList.begin());
-	}
-	
+	//	moveList.erase(moveList.begin());
+	//}
+	//
 
-	/* ********************************************************* */
-	/*                      NON-PV NODES                         */
-	/*                                                           */
-	/*              Ordinary search in non-PV nodes              */
-	/*                                                           */
-	/* ********************************************************* */	
+	///* ********************************************************* */
+	///*                      NON-PV NODES                         */
+	///*                                                           */
+	///*              Ordinary search in non-PV nodes              */
+	///*                                                           */
+	///* ********************************************************* */	
+	//for (const auto& m : moveList) {
+	//	doMove(m, copy);
+	//	mySearch.nodes++;
+	//	
+	//	short score = -pvs<false>(copy, depth - 1, -alpha - 1, -alpha, subPV);
+
+	//	if ((score > alpha) && (score < beta)) {
+	//		score = -pvs<false>(copy, depth - 1, -beta, -alpha, subPV);
+	//		
+	//		if (score > alpha) 
+	//			alpha = score;
+	//	}
+
+	//	undoMove(m, copy, p);
+
+	//	if (score > bestScore) {
+
+	//		pv[0] = m;
+	//		memcpy(pv + 1, subPV, 63 * sizeof(Move));
+	//		pv[63] = 0;
+
+	//		if (score >= beta) {
+	//			bestScore = score;
+	//			bestMove = m;
+	//			goto exit_loop;
+	//		}
+
+	//		bestScore = score;
+	//		bestMove = m;
+	//	}
+	//exit_loop:;
+	//} /*
+
+	doMove(moveList[0], copy);
+	mySearch.nodes++;
+	bestScore = -pvs<false>(copy, depth - 1, -beta, -alpha, subPV);
+	undoMove(moveList[0], copy, p);
+	bestMove = moveList[0];
+	pv[0] = bestMove;
+	memcpy(pv + 1, subPV, 63 * sizeof(Move));
+	pv[63] = 0;
+	moveList.erase(moveList.begin());
+
 	for (const auto& m : moveList) {
 		doMove(m, copy);
 		mySearch.nodes++;
-		
 		short score = -pvs<false>(copy, depth - 1, -alpha - 1, -alpha, subPV);
 
-		if ((score > alpha) && (score < beta)) {
+		if (score > alpha && score < beta)
 			score = -pvs<false>(copy, depth - 1, -beta, -alpha, subPV);
-			
-			if (score > alpha) 
-				alpha = score;
-		}
 
 		undoMove(m, copy, p);
 
-		if (score > bestScore) {
-
-			pv[0] = m;
-			memcpy(pv + 1, subPV, 63 * sizeof(Move));
-			pv[63] = 0;
-
-			if (score >= beta) {
-				bestScore = score;
-				bestMove = m;
-				goto exit_loop;
-			}
-
+		if (score >= bestScore) {
 			bestScore = score;
 			bestMove = m;
+			pv[0] = bestMove;
+			memcpy(pv + 1, subPV, 63 * sizeof(Move));
+			pv[63] = 0;
+			if (score >= alpha) alpha = score;
+			if (score >= beta) break;
 		}
-	exit_loop:;
 	}
 
 	
@@ -312,137 +344,49 @@ const short pvs(Position const& p, short const& depth, short alpha, short beta, 
 }
 
 
-template<bool nullMove>
-const short negamaxAB(Position const& p, short const& depth, short alpha, short beta, Move* pv)
+const short legacyPVS(Position const& p, short const& depth, short alpha, short beta, Move* pv)
 {
 	Position copy = p;
-	Move bestMove{}, subPV[64]{};
-	pv[0] = 0;
-	short bestScore = -MATE + (mySearch.depth - depth), alphaOrig = alpha;
-	uint32_t key = static_cast<uint32_t>(copy.getZobrist());
-	TTEntry TTEntry{};
+	Move subPV[MAX_PLY]{};
 
-	if (TT::table[key % TT_SIZE].key == key && nullMove == false) {
-		TTEntry = TT::table[key % TT_SIZE];
-		mySearch.ttHits++;
-
-		if (TT::isLegalEntry(TTEntry, copy) && TTEntry.depth >= depth) {
-
-			mySearch.ttUseful++;
-
-			switch (TTEntry.nodeType) {
-			/* case EXACT:
-				return TTEntry.score;
-				break; */
-			case LOWER_BOUND:
-				if (alpha < TTEntry.score)
-					alpha = TTEntry.score;
-				break;
-			case UPPER_BOUND:
-				if (beta > TTEntry.score)
-					beta = TTEntry.score;
-				break;
-			default:
-				break;
-			}
-
-			if (alpha >= beta) {
-				return TTEntry.score;
-			}
-		}
-	} 
-
-	if (depth <= 0)
+	if (depth == 0)
 		return quiescence(copy, alpha, beta);
-	
-	// position is not in TT, or we received only upper- or lower-bound values
-	std::vector<Move> moves = moveGeneration(copy), moveList{};
-	moves = pruneIllegal(moves, copy);
-	moveList = ordering(moves);
 
-	// null-move pruning. We only use it if side is not under check and if game
-	// is not in ending state (to avoid zugzwang issues with null-move pruning)
-	if (!underCheck(copy.getTurn(), copy) && !copy.isEnding()) {
+	std::vector<Move> moveList = moveGeneration(copy);
+	moveList = pruneIllegal(moveList, copy);
+	// moveList = ordering(moves);
 
-		// let's do a dummy (null-) move, then proceed with pruning
-		if (copy.getTurn() == BLACK)
-			copy.setMoveNumber(copy.getMoveNumber() + 1);
-
-		copy.updateZobrist(copy.getTurn());
-
-		if (copy.getTurn() == WHITE)
-			copy.setTurn(BLACK);
-		else
-			copy.setTurn(WHITE);
-		
-		copy.updateZobrist(copy.getTurn());
-
-		if (copy.getEnPassant() != SQ_EMPTY) {
-			copy.updateZobrist(copy.getEnPassant());
-			copy.setEnPassant(SQ_EMPTY);
-			copy.updateZobrist(copy.getEnPassant());
-		}
-
-		short score = -negamaxAB<true>(copy, depth - R - 1, -beta, -beta + 1, subPV);
-
-		if (score >= beta) 
-			return score;
-	}
-
-
-	/* we found an hash move with minor depth than current one, so we cannot directly use it?
-	Nonetheless, let's try the hash move first for our ordinary search  */
-	if (TTEntry.move) {
-		if (std::find(moveList.begin(), moveList.end(), TTEntry.move) != moveList.end()) {
-			std::vector<Move>::iterator it = std::find(moveList.begin(), moveList.end(), TTEntry.move);
-			std::rotate(moveList.begin(), it, it + 1);
-		}
-	}
+	doMove(moveList[0], copy);
+	mySearch.nodes++;
+	short bestScore = -legacyPVS(copy, depth - 1, -beta, -alpha, subPV);
+	undoMove(moveList[0], copy, p);
+	mySearch.bestMove = moveList[0];
+	pv[0] = mySearch.bestMove;
+	memcpy(pv + 1, subPV, 63 * sizeof(Move));
+	pv[63] = 0;
+	moveList.erase(moveList.begin());
 
 	for (const auto& m : moveList) {
 		doMove(m, copy);
 		mySearch.nodes++;
-		short score = -negamaxAB<false>(copy, depth - 1, -beta, -alpha, subPV);
+		short score = -legacyPVS(copy, depth - 1, -alpha - 1, -alpha, subPV);
+
+		if (score > alpha && score < beta)
+			score = -legacyPVS(copy, depth - 1, -beta, -alpha, subPV);
+
 		undoMove(m, copy, p);
 
-		if (score >= beta) {
+		if (score >= bestScore) {
 			bestScore = score;
-			bestMove = m;
-			break;
-		}	
-		
-		if (score > bestScore) { // Beware: unlike negamaxRoot(), here it seems better only 'greather than'
-			bestScore = score;
-			bestMove = m;
-			pv[0] = bestMove;
+			mySearch.bestMove = m;
+			pv[0] = mySearch.bestMove;
 			memcpy(pv + 1, subPV, 63 * sizeof(Move));
 			pv[63] = 0;
-
-			if (score > alpha)
-				alpha = score;
+			if (score >= alpha) alpha = score;
+			if (score >= beta) break;
 		}
 	}
 
-	/* we only update TT if there is a best move. In other words, if it's mate and therefore
-	there is not any best move (or, hence, any move at all), we do not want to update
-	TT, because otherwise we would have a dummy entry with score MATE or -MATE and move = 0, polluting TT! */
-	if (bestMove) {
-		TTEntry.score = bestScore;
-		if (bestScore <= alphaOrig)
-			TTEntry.nodeType = UPPER_BOUND;
-		else if (bestScore >= beta)
-			TTEntry.nodeType = LOWER_BOUND;
-		else
-			TTEntry.nodeType = EXACT;
-
-		TTEntry.age = static_cast<uint8_t>(copy.getMoveNumber());
-		TTEntry.depth = static_cast<uint8_t>(depth);
-		TTEntry.key = static_cast<uint32_t>(copy.getZobrist());
-		TTEntry.move = bestMove;
-		TT::Store(TTEntry);
-	}
-
-	mySearch.bestMove = bestMove; 
 	return bestScore;
 }
 

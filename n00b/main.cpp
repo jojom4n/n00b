edc04dcd707c3163a00afa2b94b4fd162bc09021
sarum4n <jojom4n@gmx.com>
2021-01-19 19:28:48 +0100

    Changes in year information

--- n00b/main.cpp
+++ n00b/main.cpp
@@ -13,7 +13,7 @@ int main()
 	/* std::thread t3(TT::init);
 	t3.detach(); */
 	std::cout << "Welcome to n00b v. 0.01 - An experimental chess engine project" << std::endl;
-	std::cout << "Copyright (c) 2019 Binary Pollution" << std::endl;
+	std::cout << "Copyright (c) 2021 Binary Pollution" << std::endl;
 	newGame();
 	return 0;
 }

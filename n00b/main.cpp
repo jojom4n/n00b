718e313c4930f809f2ed75516645ba3069a30eac
sarum4n <jojom4n@gmx.com>
2021-01-20 19:02:35 +0100

    Some cleaning

--- n00b/main.cpp
+++ n00b/main.cpp
@@ -3,15 +3,12 @@
 #include "game.h"
 #include "zobrist.h"
 #include <iostream>
-// #include <thread>
 
 
 int main()
 {
 	initAttacks();
 	Zobrist::init();
-	/* std::thread t3(TT::init);
-	t3.detach(); */
 	std::cout << "Welcome to n00b v. 0.01 - An experimental chess engine project" << std::endl;
 	std::cout << "Copyright (c) 2021 Binary Pollution" << std::endl;
 	newGame();

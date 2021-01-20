bfd4017c85e034bc4957d90e79475d4009107e1a
sarum4n <jojom4n@gmx.com>
2021-01-20 23:11:03 +0100

    Minor changes to move ordering

--- n00b/moveorder.cpp
+++ n00b/moveorder.cpp
@@ -10,42 +10,17 @@ extern const std::map<Piece, ushort> g_pieceValue; // see evaluation.cpp (and it
 
 std::vector<Move> ordering(std::vector<Move> const& moves, Position const& p)
 {
-	/* std::vector<Move> orderedList = shallowSort(moves, p);
+	std::vector<Move> moveList{};
 
-	return orderedList; */
-
-	std::vector<Move> moveList{}, moveTemp{};
+	moveList = mvv_lva(moves);
 	
-	moveTemp = mvv_lva(moves);
-	moveList.insert(moveList.begin(), moveTemp.begin(), moveTemp.end());
-
-	// add all the other generated moves but not ordered so far
-	for (auto const& elem : moves) {
-		if (std::find(moveList.begin(), moveList.end(), elem) == moveList.end())
+	for (auto elem : moves) {
+		ushort moveType = ((C64(1) << 3) - 1) & (elem >> 6); //is move a capture?
+		if (!(moveType == CAPTURE))
 			moveList.push_back(elem);
 	}
 
 	return moveList;
-}
-
-
-std::vector<Move> shallowSort(std::vector<Move> const& moves, Position const& p)
-{
-	Position copy = p;
-
-	std::vector<Move> orderedList{};
-	std::multimap<short, Move, std::less<int>> sortedMap{};
-
-	for (auto const& elem : moves) {
-		doMove(elem, copy);
-		sortedMap.insert(std::pair<short, Move>(quiescence(copy, ALPHA, BETA), elem));
-		undoMove(elem, copy, p);
-	}
-
-	for (std::multimap<short, Move>::iterator it = sortedMap.begin(); it != sortedMap.end(); ++it)
-		orderedList.push_back((*it).second);
-
-	return orderedList;
 }
 
 

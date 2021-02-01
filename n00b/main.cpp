e975c61b65ecbfd7fe3a46a759d77351c25dadd3
sarum4n <jojom4n@gmx.com>
2021-02-01 12:15:13 +0100

    Changed version number

--- n00b/main.cpp
+++ n00b/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	initAttacks();
 	Zobrist::init();
-	std::cout << "Welcome to n00b v. 0.01 - An experimental chess engine project" << std::endl;
+	std::cout << "Welcome to n00b v. 0.1 - An experimental chess engine project" << std::endl;
 	std::cout << "Copyright (c) 2021 Binary Pollution" << std::endl;
 	newGame();
 	return 0;

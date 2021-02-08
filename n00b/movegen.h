45a4cdcd9d764e6267e71213e434f123bb513c13
sarum4n <jojom4n@gmx.com>
2021-02-08 16:34:27 +0100

    Typos

--- n00b/movegen.h
+++ n00b/movegen.h
@@ -8,14 +8,14 @@ class Position;
 
 const std::vector<Move> moveGeneration(Position const &p);
 const std::vector<Move> generateOnlyKing(Color const &c, Position const &p);
-const Bitboard pawnMoves(Position const& p, Square const& from, Color const &c, Bitboard const& occ, Bitboard const& own);
-void castleMoves(Position const& p, std::vector<Move>& moveList, Color const& c, Bitboard const& occ);
-void enPassant(Position const& p, Square const& enPassant, Color const& c, std::vector<Move>& moveList);
-const MoveType setType(Piece const& piece, Bitboard const& occ, Color const& c, Square const& from, Square const& to);
-const Move composeMove(Square const& from, Square const& to, Color const& c, ushort const& p, MoveType const& type,
-	Piece const& captured, ushort const& promoteTo);
-ushort underCheck(Color const& c, Position const& p);
-void pruneIllegal(std::vector<Move>& moveList, Position& p);
-const std::vector<Move> moveGenQS(Position const& p);
+const Bitboard pawnMoves(Position const &p, Square const &from, Color const &c, Bitboard const &occ, Bitboard const &own);
+void castleMoves(Position const &p, std::vector<Move> &moveList, Color const &c, Bitboard const &occ);
+void enPassant(Position const &p, Square const &enPassant, Color const &c, std::vector<Move> &moveList);
+const MoveType setType(Piece const &piece, Bitboard const &occ, Color const &c, Square const &from, Square const &to);
+const Move composeMove(Square const &from, Square const &to, Color const &c, ushort const &p, MoveType const &type,
+	Piece const &captured, ushort const &promoteTo);
+ushort underCheck(Color const &c, Position const &p);
+void pruneIllegal(std::vector<Move> &moveList, Position &p);
+const std::vector<Move> moveGenQS(Position const &p);
 
 #endif

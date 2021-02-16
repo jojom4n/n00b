2c0d91bf8750c2a6803f09d1fb45e580cc768dcc
sarum4n <jojom4n@gmx.com>
2021-02-16 16:48:56 +0100

    Reduced PERFT_CACHE

--- src/params.h
+++ src/params.h
@@ -3,7 +3,7 @@
 
 #define TT_BIT (8000000 / 128) // MB (8 mln bits) / 128-bit TT
 
-constexpr unsigned long long PERFT_CACHE_SIZE = 0x2DC6C0; // perft cache size (in number of positions)
+constexpr unsigned long long PERFT_CACHE_SIZE = 1000000; // perft cache size (in number of positions)
 constexpr unsigned long long TT_SIZE = 64 * TT_BIT; // megabytes dedicated to TT
 
 #define MARGIN 200 // for futility pruning
